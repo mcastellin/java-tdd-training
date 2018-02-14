8c767c4771a7d053fe5ce5a292b48ea7445f8915
Manuel Castellin <castellin.manuel@gmail.com>
2018-02-14 10:22:56 +0000

    PasswordValidator excercise solution

--- src/main/java/org/training/ex2/PasswordValidator.java
+++ src/main/java/org/training/ex2/PasswordValidator.java
@@ -3,10 +3,25 @@ package org.training.ex2;
 public class PasswordValidator {
 
     public static void validate(String passwordString) throws InvalidPasswordException {
-        //TODO: implement method
+
+        if(passwordString == null) {
+            throw new IllegalArgumentException("password cannot be null");
+        }
 
         if (!(passwordString.length() >= 5)) {
-            throw new InvalidPasswordException();
+            throw new InvalidPasswordException("String must be at least 5 character long");
+        }
+
+        if(!passwordString.matches("[a-zA-Z0-9_!?@]*")){
+            throw new InvalidPasswordException("Characters allowed are a-z, a-Z, 0-9, _, !, ? or @");
+        }
+
+        if(!passwordString.matches(".*[_!?@]+.*")){
+            throw new InvalidPasswordException("Password must contain at least one of the following: _, !, ? or @");
+        }
+
+        if(!passwordString.matches(".*\\d+.*")){
+            throw new InvalidPasswordException("Password must contain at least one number character");
         }
     }
 }
--- src/test/java/org/training/ex2/PasswordValidatorTest.java
+++ src/test/java/org/training/ex2/PasswordValidatorTest.java
@@ -2,62 +2,83 @@ package org.training.ex2;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import sun.security.util.Password;
 
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.fest.assertions.Assertions.*;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnitParamsRunner.class)
 public class PasswordValidatorTest {
 
     public static Collection goodPasswords() {
         return Arrays.asList(
-                "pass123",
-                "asdfggh1"
+                "p@ss123",
+                "@sdfggh1",
+                "?iwantmom99?"
+
         );
     }
 
     public static Collection badPasswords() {
         return Arrays.asList(
                 "p123",
-                "asdfgghasdf"
+                "asdfgghasdf",
+                "p@ss",
+                "rand0m",
+                "123456789",
+                "my*pass123_"
         );
     }
 
 
     @Parameters(method = "goodPasswords")
     @Test
-    public void validateGoodPassword(String goodPassword) throws InvalidPasswordException {
+    public void validateGoodPassword(String goodPassword) {
 
-        PasswordValidator.validate(goodPassword);
+        String exceptionString = null;
+
+        try {
+            PasswordValidator.validate(goodPassword);
+        } catch (InvalidPasswordException e) {
+            exceptionString = e.getClass().getName();
+        }
+
+        assertNull(String.format("Exception %s was not expected.", exceptionString),
+                exceptionString);
 
     }
 
     @Parameters(method = "badPasswords")
     @Test(expected = InvalidPasswordException.class)
-    public void validateBadPasswords(String badPass) throws InvalidPasswordException {
+    public void validateBadPasswords(String badPassword) throws InvalidPasswordException {
 
-        PasswordValidator.validate(badPass);
+        PasswordValidator.validate(badPassword);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void validateNullPasswordShoulThrowIllegalArgument() throws InvalidPasswordException {
+
+        PasswordValidator.validate(null);
 
     }
 
     @Parameters(method = "badPasswords")
     @Test
     public void checkExceptionHasMessage(String badPass) {
+        boolean exceptionThrown = false;
         try {
             PasswordValidator.validate(badPass);
         } catch (InvalidPasswordException e) {
             assertThat(e.getMessage()).isNotEmpty();
-        }finally {
-            Assertions.fail("Exception not thrown");
+            exceptionThrown = true;
         }
 
+        assertTrue("Exception not thrown", exceptionThrown);
     }
 }
